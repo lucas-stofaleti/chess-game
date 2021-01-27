4d32ee7472e183942cbd8b3233d008d739cce73b
Lucas Stofaleti <lucastofaleti@hotmail.com>
2021-01-27 17:09:01 -0300

    Board Exception

--- src/board/Board.java
+++ src/board/Board.java
@@ -15,16 +15,42 @@ public class Board
 	
 	public Piece piece(int row, int column)
 	{
+		Position position = new Position(row,column);
+		if(!positionExists(position))
+		{
+			throw new BoardException("Posição Inválida!");
+		}
 		return pieces[row][column];
 	}
 	public Piece piece(Position position)
 	{
+		if(!positionExists(position))
+		{
+			throw new BoardException("Posição Inválida!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position)
 	{
+		if(thereIsAPiece(position))
+		{
+			throw new BoardException("Já existe uma peça!");
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean positionExists(Position position)
+	{
+		return position.getRow()<rows && position.getColumn()<columns && position.getRow()>=0 && position.getColumn()>=0;
+	}
+	public boolean thereIsAPiece(Position position)
+	{
+		if(!positionExists(position))
+		{
+			throw new BoardException("Posição Inválida!");
+		}
+		return pieces[position.getRow()][position.getColumn()] != null;
 	}
 }
